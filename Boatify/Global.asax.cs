b84ccf97eea88eeef39f58007b8e46aa5a76b09d
tanti4563 <tanti4563@gmail.com>
2025-05-21 17:26:41 +0700

    a

--- Boatify/Global.asax.cs
+++ Boatify/Global.asax.cs
@@ -5,9 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using System.Web.Security;
 
-namespace ChuyenDI
+namespace Boatify
 {
     public class MvcApplication : System.Web.HttpApplication
     {
@@ -17,25 +16,6 @@ namespace ChuyenDI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-        }
-        
-        protected void Application_PostAuthenticateRequest(Object sender, EventArgs e)
-        {
-            if (FormsAuthentication.CookiesSupported && Request.Cookies[FormsAuthentication.FormsCookieName] != null)
-            {
-                try
-                {
-                    var cookie = Request.Cookies[FormsAuthentication.FormsCookieName];
-                    var ticket = FormsAuthentication.Decrypt(cookie.Value);
-                    
-                    if (ticket != null && !ticket.Expired)
-                    {
-                        var identity = new System.Security.Principal.GenericIdentity(ticket.Name);
-                        Context.User = new System.Security.Principal.GenericPrincipal(identity, null);
-                    }
-                }
-                catch { /* Ignore any exception and proceed with default authentication */ }
-            }
         }
     }
 }
